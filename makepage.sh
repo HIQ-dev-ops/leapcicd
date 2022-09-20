ef4dd6dbd8b56513a4c89e0315921e0860716256
Sheldon Keeping <sheldonkeeping@hotmail.com>
2022-09-20 21:57:26 +0200

    Update makepage.sh

--- makepage.sh
+++ makepage.sh
@@ -1,13 +1,16 @@
+#!/bin/bash
+touch public/index.html
 
-          mkdir public
-          cat > public/index.html <<EOL
-          <!doctype html>
-          <html>
+cat > public/index.html << EOF
+
+<!DOCTYPE html>
+ <html>
             <head>
               <title>GitHub Pages deployed!</title>
             </head>
             <body>
               <p>GitHub Pages with <strong>${{ github.sha }}</strong> commit ID has been deployed through <a href="https://github.com/marketplace/actions/github-pages">GitHub Pages action</a> successfully.</p>
             </body>
-          </html>
-          EOL
+ </html>
+
+EOF
